9c3c07e3e136191ec2773796ab35451e72e588d6
TimParfenov <timonmih84@gmail.com>
2021-12-06 11:28:50 +1300

    Board:
    hasWon
    
    if(cells[2][0].content == thePlayer && cells[1][1].content == thePlayer
    && cells[0][2].content == thePlayer)
    			return true;

--- Tic-Tac-Toe/src/Board.java
+++ Tic-Tac-Toe/src/Board.java
@@ -64,7 +64,8 @@ public class Board {
 		 
 		
 		// TODO: Check the diagonal in the other direction
-		
+		if(cells[2][0].content == thePlayer && cells[1][1].content == thePlayer && cells[0][2].content == thePlayer)
+			return true;
 
 		
 		//no winner, keep playing
