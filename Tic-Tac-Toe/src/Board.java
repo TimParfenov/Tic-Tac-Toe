88bdfb8bc00d2781ae15652a9309b67f09e1cc73
TimParfenov <timonmih84@gmail.com>
2021-12-06 10:23:16 +1300

    Board:
    Added
    cells = new Cell[GameMain.ROWS] [GameMain.COLS];

--- Tic-Tac-Toe/src/Board.java
+++ Tic-Tac-Toe/src/Board.java
@@ -13,11 +13,13 @@ public class Board {
 	public Board() {
 		
 	 //TODO: initialise the cells array using ROWS and COLS constants 
+//		!!!!!!!!
 
 		
 		for (int row = 0; row < GameMain.ROWS; ++row) {
 			for (int col = 0; col < GameMain.COLS; ++col) {
 				cells[row][col] = new Cell(row, col);
+				cells = new Cell[GameMain.ROWS] [GameMain.COLS];
 			}
 		}
 	}
