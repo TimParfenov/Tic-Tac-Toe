a18eef5f00dfd7d912a17609cb71a86b30775e02
TimParfenov <timonmih84@gmail.com>
2021-12-06 12:13:42 +1300

    GameMain:
    GameMain
    
    addMouseListener(this);

--- Tic-Tac-Toe/src/GameMain.java
+++ Tic-Tac-Toe/src/GameMain.java
@@ -40,7 +40,7 @@ public class GameMain extends JPanel implements MouseListener{
 	public GameMain() {   
 		
 		// TODO: This JPanel fires a MouseEvent on MouseClicked so add required event listener to 'this'.          
-	    
+	    addMouseListener(this);
 	    
 		// Setup the status bar (JLabel) to display status message       
 		statusBar = new JLabel("         ");       
